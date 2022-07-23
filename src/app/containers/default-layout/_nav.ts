4c06adeb08cf37d134b003830f72f5fe5de268f9
spyrax10 <smarteman10@gmail.com>
2022-07-23 12:59:40 +0800

    Temporarily Disable unused Side Menu Modules

--- src/app/containers/default-layout/_nav.ts
+++ src/app/containers/default-layout/_nav.ts
@@ -11,105 +11,105 @@ export const navItems: INavData[] = [
     url: '/sales',
     iconComponent: { name: 'cil-chart-line' }
   },
-  {
-    name: 'Purchases',
-    url: '/purchases',
-    iconComponent: { name: 'cil-basket' }
-  },
+  // {
+  //   name: 'Purchases',
+  //   url: '/purchases',
+  //   iconComponent: { name: 'cil-basket' }
+  // },
 
-  {
-    name: 'Inventory',
-    url: '/inventory',
-    iconComponent: { name: 'cil-chart' }
-  },
+  // {
+  //   name: 'Inventory',
+  //   url: '/inventory',
+  //   iconComponent: { name: 'cil-chart' }
+  // },
 
-  //Reports Section
-  {
-    title: true,
-    name: 'Reports'
-  },
-  {
-    name: 'Sales',
-    url: '/sales/reports',
-    iconComponent: { name: 'cil-chart-line' },
-    children : [
-      {
-        name: 'Daily Sales Report'
-      },
-      {
-        name: 'Daily Sales Summary Report'
-      },
-      {
-        name: 'Grab Foods'
-      },
-      {
-        name: 'GCASH'
-      },
-      {
-        name: 'CC Transactions'
-      },
-      {
-        name: 'Guide For Difference'
-      },
-      {
-        name: 'Sales Report'
-      },
-      {
-        name: 'Sales Analysis'
-      },
-      {
-        name: 'Sales vs Inventory Usage'
-      },
-      {
-        name: 'Consolidated Sales Summary Report'
-      }
-    ]
+  // //Reports Section
+  // {
+  //   title: true,
+  //   name: 'Reports'
+  // },
+  // {
+  //   name: 'Sales',
+  //   url: '/sales/reports',
+  //   iconComponent: { name: 'cil-chart-line' },
+  //   children : [
+  //     {
+  //       name: 'Daily Sales Report'
+  //     },
+  //     {
+  //       name: 'Daily Sales Summary Report'
+  //     },
+  //     {
+  //       name: 'Grab Foods'
+  //     },
+  //     {
+  //       name: 'GCASH'
+  //     },
+  //     {
+  //       name: 'CC Transactions'
+  //     },
+  //     {
+  //       name: 'Guide For Difference'
+  //     },
+  //     {
+  //       name: 'Sales Report'
+  //     },
+  //     {
+  //       name: 'Sales Analysis'
+  //     },
+  //     {
+  //       name: 'Sales vs Inventory Usage'
+  //     },
+  //     {
+  //       name: 'Consolidated Sales Summary Report'
+  //     }
+  //   ]
 
-  },
+  // },
 
-  {
-    name: 'Purchases',
-    url: '/purchases',
-    iconComponent: { name: 'cil-basket' },
-    children : [
-      {
-        name: 'WOF (Weekly Order Form)'
-      },
-      {
-        name: 'Consolidated  Delivery Summary Report'
-      },
-      {
-        name: 'Consolidated Purchase Summary Report'
-      },
-      {
-        name: 'Invoice Document'
-      },
-      {
-        name: 'Consolidated Finish Goods Summary Report'
-      },
-    ]
-  },
-  {
-    name: 'Inventory',
-    url: '/inventory',
-    iconComponent: { name: 'cil-chart' },
-    children : [
-      {
-        name: 'Daily Inventory Movement Per Branch'
-      },
-      {
-        name: 'Inventory Count Per Day'
-      },
-      {
-        name: 'Consolidated Purchase Summary Report'
-      },
-      {
-        name: 'Inventory Variance Report Per Branch'
-      },
-      {
-        name: 'Consolidated Inventory Summary Report'
-      },
-    ]
-  },
+  // {
+  //   name: 'Purchases',
+  //   url: '/purchases',
+  //   iconComponent: { name: 'cil-basket' },
+  //   children : [
+  //     {
+  //       name: 'WOF (Weekly Order Form)'
+  //     },
+  //     {
+  //       name: 'Consolidated  Delivery Summary Report'
+  //     },
+  //     {
+  //       name: 'Consolidated Purchase Summary Report'
+  //     },
+  //     {
+  //       name: 'Invoice Document'
+  //     },
+  //     {
+  //       name: 'Consolidated Finish Goods Summary Report'
+  //     },
+  //   ]
+  // },
+  // {
+  //   name: 'Inventory',
+  //   url: '/inventory',
+  //   iconComponent: { name: 'cil-chart' },
+  //   children : [
+  //     {
+  //       name: 'Daily Inventory Movement Per Branch'
+  //     },
+  //     {
+  //       name: 'Inventory Count Per Day'
+  //     },
+  //     {
+  //       name: 'Consolidated Purchase Summary Report'
+  //     },
+  //     {
+  //       name: 'Inventory Variance Report Per Branch'
+  //     },
+  //     {
+  //       name: 'Consolidated Inventory Summary Report'
+  //     },
+  //   ]
+  // }
 
 ];
